3e6472968ac82bf54795b04e01f36226a20e07e4
Benjamin Krech <krech.benjamin@gmail.com>
2026-05-05 19:33:47 +0200

    refactor: moved app-shell from planer to general components to match it's scope

--- app/(planner)/layout.tsx
+++ app/(planner)/layout.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import type { ReactNode } from "react";
 
-import { AppShell } from "@/features/planner/components/layout/app-shell";
+import { AppShell } from "@/components/layout/app-shell";
 import { AuthGuard } from "@/features/auth/components/auth-guard";
 import { SidebarContent } from "@/components/layout/sidebar-content";
 
